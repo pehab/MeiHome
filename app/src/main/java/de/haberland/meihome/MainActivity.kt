f80522d935e6d0f2afeb0a17ab270316eb74cdac
pehab <65153752+pehab@users.noreply.github.com>
2026-09-22 20:33:11 +0200

    Wall-Tablet im Betrieb wach halten

--- app/src/main/java/de/haberland/meihome/MainActivity.kt
+++ app/src/main/java/de/haberland/meihome/MainActivity.kt
@@ -1,6 +1,7 @@
 package de.haberland.meihome
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,8 @@ import de.haberland.meihome.ui.MeiHomeApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         enableEdgeToEdge()
         WindowCompat.setDecorFitsSystemWindows(window, false)
