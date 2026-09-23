54fdd00b273f121534fc88dcbe1b773a8bfbfab7
pehab <65153752+pehab@users.noreply.github.com>
2026-09-23 09:03:23 +0200

    Material Icons für Settings ergänzen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,5 +33,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
