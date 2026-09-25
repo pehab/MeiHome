aca11fb398189cffcdb414c53269bff0cd9bf66b
pehab <65153752+pehab@users.noreply.github.com>
2026-09-25 17:39:26 +0200

    MeiHome auf Version 0.4.0 erhöhen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.haberland.meihome"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     compileOptions {
