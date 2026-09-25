5425b4094a4203a442f6c1f65f15569527da36ea
pehab <65153752+pehab@users.noreply.github.com>
2026-09-25 10:29:02 +0200

    Lifecycle Compose für StateFlow nutzen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation("androidx.fragment:fragment-ktx:1.8.9")
